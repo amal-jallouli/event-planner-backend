e1ca825a03abee6eb58a6c673b24d5c5c2bec30e
amal-jallouli <jallouliamal754@gmail.com>
2026-05-07 23:54:35 +0100

    Fix CORS for Vercel

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,12 @@
 return [
     'paths'               => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'     => ['*'],
-    'allowed_origins'     => ['http://localhost:4200'],
+    'allowed_origins' => [
+    'http://localhost:4200',
+    'https://event-planner-front.vercel.app',
+    'https://event-planner-front-9jyfz8qnt-amal-jalloulis-projects.vercel.app',
+    'https://*.vercel.app',
+],
     'allowed_origins_patterns' => [],
     'allowed_headers'     => ['*'],
     'exposed_headers'     => [],
